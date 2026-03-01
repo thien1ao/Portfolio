6516828360aa713b338fe5f161e667a1c6980dce
mike <mike@Mike.debianbtw>
2026-03-01 13:24:39 +0000

    stars_meteors

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(_dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
